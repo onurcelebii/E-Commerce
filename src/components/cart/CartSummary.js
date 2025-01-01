68cce5e895c99b72a8a755fd59a6e3a8f503dcbd
Onur Celebi <onurcelebi10@hotmail.com>
2025-01-01 17:50:08 +0300

    Sepet tutarı 2 ondalık basamağa yuvarlandı

--- src/components/cart/CartSummary.js
+++ src/components/cart/CartSummary.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../../styles/CartSummary.css"
+import "../../styles/CartSummary.css";
 
 function CartSummary({ total, handleClearCart }) {
+  const formattedTotal = total.toFixed(2); // 2 ondalık basamağa yuvarlama
+
   return (
     <div className="cart-summary mt-4 p-4 border rounded-3 shadow-sm">
       <h4 className="text-center mb-3">
-        Total: <span className="text-success">{total} €</span>
+        Total: <span className="text-success">{formattedTotal} €</span>
       </h4>
       <div className="d-flex justify-content-between">
         <button
