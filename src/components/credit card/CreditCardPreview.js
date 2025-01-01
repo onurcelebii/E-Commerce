1d481a039dd5d20fb9a01b157f1ff32432577a9a
Onur Celebi <onurcelebi10@hotmail.com>
2025-01-01 19:08:03 +0300

    Kredi kartı önizlemesi değiştirildi

--- src/components/credit card/CreditCardPreview.js
+++ src/components/credit card/CreditCardPreview.js
@@ -4,54 +4,90 @@ function CreditCardPreview({ cardholderName, cardNumber, expiryDate, cvv }) {
   return (
     <div
       className="card p-3 border-0 shadow"
-      style={{ maxWidth: "350px", borderRadius: "10px" }}
+      style={{
+        maxWidth: "600px",
+        width: "100%",
+        borderRadius: "15px",
+        backgroundColor: "#1a2a3a",
+        color: "#fff",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.3)",
+      }}
     >
       <div className="card-body">
-        <div className="text-center">
-          <div
-            className="card"
-            style={{
-              width: "100%",
-              borderRadius: "10px",
-              backgroundColor: "#2c3e50",
-              color: "#fff",
-              padding: "20px",
-              fontSize: "16px",
-            }}
-          >
-            <div className="d-flex justify-content-between">
-              <div>
-                <span>VISA</span>
-              </div>
-              <div>
-                <span
-                  className="badge bg-light text-dark"
-                  style={{ padding: "5px 10px", borderRadius: "20px" }}
-                >
-                  Debit
-                </span>
-              </div>
+        <div
+          className="d-flex flex-column justify-content-between"
+          style={{ height: "250px" }}
+        >
+          {/* Kredi Kartı Üst Kısmı */}
+          <div className="d-flex justify-content-between">
+            <div>
+              <span style={{ fontSize: "18px", fontWeight: "bold" }}>VISA</span>
             </div>
-            <div
-              className="mt-4"
-              style={{ fontSize: "24px", letterSpacing: "1px" }}
-            >
-              <div>{cardNumber || "#### #### #### ####"}</div>
-            </div>
-            <div className="d-flex justify-content-between mt-4">
-              <div>
-                <span>Expires</span>
-                <div>{expiryDate || "MM/YY"}</div>
-              </div>
-              <div>
-                <span>CVV</span>
-                <div>{cvv || "***"}</div>
-              </div>
-            </div>
-            <div className="mt-4" style={{ fontSize: "18px" }}>
-              <div>{cardholderName || "Cardholder Name"}</div>
+            <div>
+              <span
+                className="badge bg-warning text-dark"
+                style={{
+                  fontSize: "12px",
+                  padding: "5px 15px",
+                  borderRadius: "25px",
+                }}
+              >
+                Debit
+              </span>
             </div>
           </div>
+
+          {/* Kredi Kartı Numarası */}
+          <div
+            style={{
+              fontSize: "30px",
+              letterSpacing: "2px",
+              marginTop: "20px",
+              fontWeight: "bold",
+              lineHeight: "1.5",
+            }}
+          >
+            <div>{cardNumber || "#### #### #### ####"}</div>
+          </div>
+
+          {/* Son Kullanma Tarihi ve CVV */}
+          <div className="d-flex justify-content-between mt-4">
+            <div>
+              <span style={{ fontSize: "12px" }}>Expires</span>
+              <div style={{ fontSize: "16px" }}>{expiryDate || "MM/YY"}</div>
+            </div>
+            <div>
+              <span style={{ fontSize: "12px" }}>CVV</span>
+              <div style={{ fontSize: "16px" }}>{cvv || "***"}</div>
+            </div>
+          </div>
+
+          {/* Kart Sahibi Adı */}
+          <div
+            style={{
+              fontSize: "16px",
+              marginTop: "15px",
+              fontWeight: "normal",
+            }}
+          >
+            {cardholderName || "Cardholder Name"}
+          </div>
+        </div>
+
+        {/* Kartın Alt Kısmı - Grafik */}
+        <div
+          style={{
+            position: "absolute",
+            bottom: "20px",
+            right: "20px",
+            // opacity: 0.2,
+          }}
+        >
+          <img
+            src="https://www.provendata.com/wp-content/uploads/2023/09/What-is-EMV-credit-card-technology.png"
+            alt="chip"
+            style={{ width: "40px" }}
+          />
         </div>
       </div>
     </div>
