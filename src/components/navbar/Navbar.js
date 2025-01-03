f278c9bd18a271323504d7301c58709e8d90b857
Onur Celebi <onurcelebi10@hotmail.com>
2025-01-03 19:34:30 +0300

    Navbar CSS güncellendi

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,31 +10,34 @@ import "../../styles/Navbar.css";
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/home">
-            <Logo /> {/* Logo bileşenini burada kullan */}
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+      <div className="navbar-container">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/home">
+              <Logo />
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
 
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav w-100 d-flex justify-content-between">
-              <SearchBar />
-              <CartIcon />
-            </ul>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav w-100 d-flex justify-content-between">
+                <SearchBar />
+                <CartIcon />
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
+
       <CategoriesMenu />
     </>
   );
