da76319c2736358bdeafbab0386cc3c1ff389729
Onur Celebi <onurcelebi10@hotmail.com>
2025-01-04 18:58:06 +0300

    Navbar responsive design eklendi

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,21 @@
-// src/components/navbar/Navbar.js
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "./SearchBar";
 import CartIcon from "./CartIcon";
 import CategoriesMenu from "./CategoriesMenu";
 import { Link } from "react-router-dom";
-import Logo from "./Logo"; // Logo bileşenini içe aktar
+import Logo from "./Logo";
 import "../../styles/Navbar.css";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
-      <div className="navbar-container">
+      <div className="navbar-container web-navbar">
         <nav className="navbar navbar-expand-lg navbar-light">
           <div className="container-fluid">
             <Link className="navbar-brand" to="/home">
@@ -24,11 +29,15 @@ const Navbar = () => {
               aria-controls="navbarNav"
               aria-expanded="false"
               aria-label="Toggle navigation"
+              onClick={toggleMenu}
             >
               <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div className="collapse navbar-collapse" id="navbarNav">
+            <div
+              className={`collapse navbar-collapse ${isMenuOpen ? "show" : ""}`}
+              id="navbarNav"
+            >
               <ul className="navbar-nav w-100 d-flex justify-content-between">
                 <SearchBar />
                 <CartIcon />
@@ -36,9 +45,49 @@ const Navbar = () => {
             </div>
           </div>
         </nav>
+        <CategoriesMenu />
       </div>
 
-      <CategoriesMenu />
+      {/* Mobil için navbar */}
+      <div className="navbar-container mobile-navbar">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/home">
+              <Logo />
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavMobile"
+              aria-controls="navbarNavMobile"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              onClick={toggleMenu}
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div
+              className={`collapse navbar-collapse ${isMenuOpen ? "show" : ""}`}
+              id="navbarNavMobile"
+            >
+              <CategoriesMenu />
+            </div>
+          </div>
+        </nav>
+
+        <div className="mobile-navbar-fixed">
+          <ul className="navbar-nav">
+            <li>
+              <SearchBar className="search-bar" />
+            </li>
+            <li>
+              <CartIcon className="cart-icon" />
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
